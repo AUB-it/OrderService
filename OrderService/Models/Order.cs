967d7f92d9243c8e50ecb4f34b53bb7f55c7ae3f
Umutnaz <umutnaz@outlook.dk>
2026-04-17 09:20:50 +0200

    Slettet nogle ligegyldige filer, tilføjet order models

---
+++ OrderService/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace OrderService.Models;
+
+
+public class Order
+{
+    public Guid Orderid  { get; set; } //id tælder som navn
+    public DateTime OrderStartdate { get; set; } //oprettelsesdato
+    public DateTime OrderEndDate  { get; set; } //order slut dato
+    public List<Orderitem> Items { get; set; } //liste af produkters id
+    public int Touserid { get; set; } //Hvem er order til
+    public int TotalPrice { get; set; } //samlet pris for alle varene
+}
+public class Orderitem
+{
+    public Guid Id { get; set; }
+    public int amount { get; set; }
+    public decimal price { get; set; }
+    public string Name { get; set; }
+}
